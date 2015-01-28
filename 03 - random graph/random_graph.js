4dd53f594791d1e60bfcddaf650583cb6431f1a7
Taylus <dragondude@gmail.com>
2015-01-27 22:33:17 -0600

    Random graph generation example

---
+++ 03 - random graph/random_graph.js
@@ -0,0 +1,40 @@
+(function() {
+    "use strict";
+    
+    function randomElementFrom(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    
+    var graph = {
+        nodes: [],
+        edges: []
+    };
+    
+    for (var i = 0; i < 100; i++) {
+        graph.nodes.push({
+            id: 'n' + i,
+            label: 'Node #' + i,
+            x: Math.random(),
+            y: Math.random(),
+            size: 1,
+            color: '#f00'
+        });
+    }
+    
+    for (i = 0; i < 100; i++) {
+        graph.edges.push({
+            id: 'e' + i,
+            source: randomElementFrom(graph.nodes).id,
+            target: randomElementFrom(graph.nodes).id,
+            color: '#bbb'
+        });
+    }
+    
+    var s = new sigma({
+      graph: graph,
+      container: 'graphContainer',
+      settings: {
+            sideMargin: 0.05
+        }
+    });
+}());
